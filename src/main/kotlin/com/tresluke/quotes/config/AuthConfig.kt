76dd62fac58893b189ceac51dee0954c5b1334d3
JyotiBhambhu <jyotic26@gmail.com>
2026-05-03 13:37:17 +0530

    chore: added auth in swagger

--- src/main/kotlin/com/tresluke/quotes/config/AuthConfig.kt
+++ src/main/kotlin/com/tresluke/quotes/config/AuthConfig.kt
@@ -1,5 +1,9 @@
 package com.tresluke.quotes.config
 
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -10,4 +14,18 @@ class AuthConfig {
 
     @Bean
     fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+    @Bean
+    fun openAPI(): OpenAPI = OpenAPI()
+        .addSecurityItem(SecurityRequirement().addList("Bearer"))
+        .components(
+            Components().addSecuritySchemes(
+                "Bearer",
+                SecurityScheme()
+                    .name("Bearer")
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")
+            )
+        )
 }
